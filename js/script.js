50c262a43c888f96d5e3b5b7f8da68336a430645
Volodymyr <101939818+aragorn-ua@users.noreply.github.com>
2024-11-06 20:26:50 +0200

    cycles06

--- js/script.js
+++ js/script.js
@@ -8,13 +8,25 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 }
+if (numberOfFilms<10) {
+    console.log('Просмотрено довольно мало фильмов');
+    } else if (numberOfFilms>=10 && numberOfFilms<=30) {
+        console.log('Вы классический зритель');
+    } else if (numberOfFilms>30) {
+        console.log('Вы киноман');
+    }  else {console.log('Произошла ошибка');}
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i=0; i<2;i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b !='' && a.length < 50) {
+ personalMovieDB.movies[a] = b;
+ console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+}
 console.log(numberOfFilms);
 console.log(personalMovieDB);
